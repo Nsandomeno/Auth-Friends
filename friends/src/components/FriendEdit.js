d273aedb03228970b1684d898c19725e0c0820be
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-29 10:34:12 -0500

    More stretch, having trouble with multiple updates at the same PUT request

--- friends/src/components/FriendEdit.js
+++ friends/src/components/FriendEdit.js
@@ -3,39 +3,92 @@ import { Link, Redirect } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 
 function FriendEdit(props) {
-const [friend, findFriend] = useState()
+const [friend, setFriend] = useState({
+    name:  '',
+    age: '',
+    email: ''
+})
+
+useEffect(() => {
+    console.log(props)
+    axiosWithAuth().get(`/api/friends/${props.match.params.id}`)
+        .then((response) => {
+            console.log("This is response from FriendEdit:", response)
+            setFriend({
+                name: response.data.name,
+                age: response.data.age,
+                email: response.data.email
+            })
+
+        })
+        .catch((error) => {
+            console.log("This is error from friend Edit")
+        })
+},[])
+
+const clear = (event) => {
+    event.preventDefault();
+    setFriend({
+        name: '',
+        age: '',
+        email: ''
+    })
+}
+
+const handleChange = (event) => {
+    setFriend({
+        [event.target.name]: event.target.value
+    })
+}
+
+const saveEdit = (event, id, editInfo) => {
+    event.preventDefault();
+    axiosWithAuth().put(`/api/friends/${id}`, editInfo)
+        .then((response) => {
+            console.log("This is the respone from save edit:", response)
+            props.history.push('/api/friends')
+        })
+        .catch((error) => {
+            console.log("This is the error from save edit:", error)
+        })
+} 
 
 
 
     return (
+        
         <div>
             <form>
             <label> Name: 
                         <input
                         name="name"
                         placeholder="Enter Name"
-
+                        value={friend.name}
+                        onChange={handleChange}
                         />
                     </label>
                     <label> Age:
                         <input
                         name="age"
                         placeholder="Enter Age"
-
-
+                        value={friend.age}
+                        onChange={handleChange}
                         />
                     </label>
                     <label> Email
                         <input
                         name="email"
                         placeholder="Enter Email"
-
-
+                        value={friend.email}
+                        onChange={handleChange}
                         />
                     </label>
             </form>
-            <button>
+            <button onClick={(event) => saveEdit(event, props.match.params.id, friend)}>
                 Save
+            </button>
+            <button onClick={clear}>
+                Clear
             </button>
             <button>
                 Delete
